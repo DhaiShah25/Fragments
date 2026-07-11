2935fc9d37e2342c4599d9aaddca1ccb3297dcf3
Dhairya Shah <dhaishah7+git@gmail.com>
2026-07-11 19:32:42 -0400

    Dashing, modularization, & wall collisions

--- src/environment.c
+++ src/environment.c
@@ -2,18 +2,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
+constexpr size_t MAP_SIZE = 15;
+constexpr float TILE_SIZE = 32.0;
+
 typedef enum GameStage : uint8_t {
+  GUI,
   // Starting Animation
   Animation,
   // Tutorial
   Movement,
-  Escape,
   Attacks,
-  Effects,
-  Weaknesses,
-  CastTime,
-  Merging,
+  Escape,
   // Gameplay
+  FloorChange,
   Floors,
   Victory,
   Loss
@@ -24,35 +25,50 @@ typedef enum TileType {
   Bookshelf,
 } TileType;
 
-Vector2 gen_map(uint16_t *map) {
-  for (size_t i = 0; i < 15; i++) {
-    map[i] = -1;
+Vector2 gen_map(uint8_t map[MAP_SIZE][MAP_SIZE]) {
+  for (size_t i = 0; i < MAP_SIZE; i++) {
+    for (size_t j = 0; j < MAP_SIZE; j++) {
+      map[i][j] = 1;
+    }
   }
-  map[1] = 0x4001;
+  for (size_t j = 0; j < MAP_SIZE - 1; j++) {
+    map[1][j] = 0;
+  }
+  map[1][0] = 1;
+  map[1][MAP_SIZE - 1] = 1;
 
-  size_t x = 1;
-  size_t y = 1;
-
-  for (size_t i = 3; i < 14; i += 2) {
+  for (size_t i = 3; i < MAP_SIZE - 1; i += 2) {
     size_t row_start = 1;
     size_t row_size = 1;
-    for (size_t j = row_start; j < 14; j += 2) {
-      map[i] &= ~(1 << j);
-      if (GetRandomValue(0, 1) == 0 && j != 13) {
-        map[i] &= ~(1 << (j + 1));
-        if (GetRandomValue(0, 10) == 10) {
-          x = i;
-          y = j;
-        }
+    for (size_t j = row_start; j < MAP_SIZE - 1; j += 2) {
+      map[i][j] = 0;
+      if (GetRandomValue(0, 1) == 0 && j != MAP_SIZE - 2) {
+        map[i][j + 1] = 0;
         row_size++;
       } else {
         size_t col = row_start + 2 * GetRandomValue(0, row_size - 1);
-        map[i - 1] &= ~(1 << col);
+        map[i - 1][col] = 0;
         row_size = 1;
         row_start = j + 2;
       }
     }
   }
 
-  return (Vector2){x * 160.0, y * 160.0};
+  size_t x = (GetRandomValue(1, 6) * 2 + 1) * TILE_SIZE + TILE_SIZE / 2;
+  size_t y = (GetRandomValue(1, 6) * 2 + 1) * TILE_SIZE + TILE_SIZE / 2;
+
+  return (Vector2){x, y};
+}
+
+void draw_map(uint8_t map[MAP_SIZE][MAP_SIZE], Vector2 fragment_location) {
+  // TODO: Stop copying the 2D map array
+  for (int x = 0; x < MAP_SIZE; x++) {
+    for (int y = 0; y < MAP_SIZE; y++) {
+      if (map[x][y]) {
+        DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, (Color){100, 20, 20, 255});
+      } else
+        DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, (Color){40, 20, 20, 255});
+    }
+  }
+  DrawCircleV(fragment_location, TILE_SIZE / 4, (Color){245, 235, 235, 255});
 }
--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "environment.c"
@@ -18,19 +19,23 @@ typedef struct Sprite {
   // Texture2D texture;
   Color color;
   Vector2 velocity;
+  float speedMultiplier;
 } Sprite;
 
-Font iosevka;
+Font iosevka40;
+Font iosevka20;
 
 void DrawLabel(const char *text, Vector2 pos, float fontSize, Color color) {
-  DrawTextEx(iosevka, text, pos, fontSize, 0, color);
+  if (fontSize > 30)
+    DrawTextEx(iosevka40, text, pos, fontSize, 0, color);
+  else
+    DrawTextEx(iosevka20, text, pos, fontSize, 0, color);
 }
 
-void draw_start_animation(float *timePassed, GameStage *stage);
+bool draw_start_animation(float timePassed);
 
-void move_player(Sprite *player) {
+void move_player(Sprite *player, uint8_t map[MAP_SIZE][MAP_SIZE]) {
   player->velocity = (Vector2){0, 0};
-  // TODO: Normalize Player Movement Velocity
   if (IsKeyDown(KEY_A)) {
     player->velocity.x -= 1;
   }
@@ -44,145 +49,193 @@ void move_player(Sprite *player) {
     player->velocity.y += 1;
   }
 
-  // Vector2 touch_pos = GetTouchPosition(0);
-  // if (touch_pos.x != -1 && touch_pos.y != -1 && player->velocity.x == 0 && player->velocity.y == 0)
-  //   player->velocity = Vector2Subtract(touch_pos, (Vector2){player->rect.x, player->rect.y});
-
   player->velocity = Vector2Normalize(player->velocity);
-  player->rect.x += player->velocity.x * GetFrameTime() * 100;
-  player->rect.y += player->velocity.y * GetFrameTime() * 100;
 
-  // if (player->rect.x + player->rect.width > CanvasWidth) {
-  //   player->rect.x = CanvasWidth - player->rect.width;
-  // } else if (player->rect.x < 0) {
-  //   player->rect.x = 0;
-  // }
-  // if (player->rect.y + player->rect.height > CanvasHeight) {
-  //   player->rect.y = CanvasHeight - player->rect.height;
-  // } else if (player->rect.y < 0) {
-  //   player->rect.y = 0;
-  // }
+  float factor = 100 * GetFrameTime() * player->speedMultiplier;
+
+  float new_x = player->rect.x + player->velocity.x * factor;
+  float new_y = player->rect.y + player->velocity.y * factor;
+
+  int left = new_x / TILE_SIZE;
+  int top = player->rect.y / TILE_SIZE;
+  int right = (new_x + player->rect.width) / TILE_SIZE;
+  int bottom = (player->rect.y + player->rect.height) / TILE_SIZE;
+
+  if (!(map[left][top] || map[right][top] || map[left][bottom] || map[right][bottom])) {
+    player->rect.x = new_x;
+  }
+
+  left = player->rect.x / TILE_SIZE;
+  top = new_y / TILE_SIZE;
+  right = (player->rect.x + player->rect.width) / TILE_SIZE;
+  bottom = (new_y + player->rect.height) / TILE_SIZE;
+
+  if (!(map[left][top] || map[right][top] || map[left][bottom] || map[right][bottom])) {
+    player->rect.y = new_y;
+  }
+  player->speedMultiplier = 1.0;
 }
 
 int main(void) {
   SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
   InitWindow(CanvasWidth, CanvasHeight, "Raylib 6.x Game Jam");
 
-  iosevka = LoadFontEx("resources/Iosevka.ttf", 40, nullptr, 0);
+  iosevka40 = LoadFontEx("resources/Iosevka.ttf", 40, nullptr, 0);
+  iosevka20 = LoadFontEx("resources/Iosevka.ttf", 20, nullptr, 0);
 
   int currentFps = 60;
-  GameStage stage = Movement;
-  uint16_t map[15] = {0};
-  int floor = -1;
+  GameStage stage = GUI;
+  uint8_t map[MAP_SIZE][MAP_SIZE] = {0};
+  int floor = 0;
   Vector2 fragment_location;
+  bool open_inventory = false;
 
-  Sprite player = {.rect = (Rectangle){200, 200, 40, 40}, .color = (Color){40, 120, 40, 255}};
+  Sprite player = {.rect = (Rectangle){TILE_SIZE, TILE_SIZE, TILE_SIZE / 4, TILE_SIZE / 4},
+                   .color = (Color){40, 120, 40, 255}};
 
   Camera2D camera = {0};
   camera.target = (Vector2){220, 220};
   camera.offset = (Vector2){360, 360};
   camera.rotation = 0.0f;
-  camera.zoom = 1.0f;
+  camera.zoom = 2.0f;
 
   float timePassed = 0.0f;
 
   SetTargetFPS(currentFps);
 
   while (!WindowShouldClose()) {
-    if (stage >= Movement)
-      move_player(&player);
+    if (stage >= Movement && stage != FloorChange)
+      move_player(&player, map);
+
+    if (IsKeyPressed(KEY_E)) {
+      open_inventory = !open_inventory;
+    }
 
     // Drawing
     BeginDrawing();
-    ClearBackground(BLACK);
+    if (stage != Animation)
+      ClearBackground(BLACK);
+    else
+      ClearBackground((Color){40, 20, 20, 255});
+
+    if (stage > Animation && stage < Victory && stage != FloorChange) {
+      camera.target = (Vector2){player.rect.x + 20, player.rect.y + 20};
+      BeginMode2D(camera);
+      draw_map(map, fragment_location);
+      DrawRectangleRec(player.rect, player.color);
+      DrawRectangleRec(player.rect, player.color);
+      EndMode2D();
+
+      if (CheckCollisionPointCircle((Vector2){player.rect.x, player.rect.y}, fragment_location, TILE_SIZE / 4)) {
+        stage = FloorChange;
+        fragment_location = gen_map(map);
+        timePassed = 0;
+
+        player.rect.x = TILE_SIZE;
+        player.rect.y = TILE_SIZE;
+
+        if (floor == 9) {
+          stage = Victory;
+        }
+      }
+    }
 
     switch (stage) {
+      case GUI:
+        Rectangle tutRec = {290, 310, 150, 60};
+        DrawRectangleRec(tutRec, (Color){20, 20, 20, 255});
+        DrawLabel("Tutorial", (Vector2){300, 320}, 40, RAYWHITE);
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), tutRec)) {
+          stage = Animation;
+        }
+
+        Rectangle gameRect = {210, 390, 340, 60};
+        DrawRectangleRec(gameRect, (Color){20, 20, 20, 255});
+        DrawLabel("Straight To Gameplay", (Vector2){220, 400}, 40, RAYWHITE);
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), gameRect)) {
+          fragment_location = gen_map(map);
+          stage = Floors;
+        }
+        break;
       case Animation:
         timePassed += GetFrameTime();
-        draw_start_animation(&timePassed, &stage);
-        break;
-      case Movement:
-        DrawRectangleRec(player.rect, player.color);
-        if (player.rect.x != 200 || player.rect.y != 200) {
+        if (draw_start_animation(timePassed)) {
+          timePassed = 0;
           stage++;
           fragment_location = gen_map(map);
-          TraceLog(LOG_INFO, "Fragment Location: %f, %f", fragment_location.x, fragment_location.y);
-        } else
-          DrawLabel("Use WASD keys to move around", (Vector2){0, 0}, 40, RAYWHITE);
-        break;
-      case Escape:
-        camera.target = (Vector2){player.rect.x + 20, player.rect.y + 20};
-        BeginMode2D(camera);
-        for (int x = 0; x < 15; x++) {
-          for (int y = 0; y < 15; y++) {
-            if (map[x] & 1 << y) {
-              DrawRectangle(x * 160, y * 160, 160, 160, (Color){100, 20, 20, 255});
-            } else
-              DrawRectangle(x * 160, y * 160, 160, 160, (Color){40, 20, 20, 255});
-          }
         }
-        DrawRectangleRec(player.rect, player.color);
-        EndMode2D();
-        DrawLabel("You have to collect the picture frame\nfragment in order to move to the next room", (Vector2){0, 0},
-                  40, RAYWHITE);
+        break;
+      case Movement:
+        if (player.rect.x != TILE_SIZE || player.rect.y != TILE_SIZE) {
+          stage++;
+        }
+        DrawLabel("Use WASD keys to move around and right\nclick to dash", (Vector2){0, 0}, 40, RAYWHITE);
         break;
       case Attacks:
-        DrawLabel("Use the JKL keys or the buttons on the bottom\nright to "
-                  "attack in the direction you are\nmoving",
+        DrawLabel("Left click or use the button on the bottom to attack\n"
+                  "in the direction you are\nmoving. You can only have \n"
+                  "1 spell equipped at a time.",
                   (Vector2){0, 0}, 40, RAYWHITE);
-        floor++;
         break;
-      case Effects:
+      case Escape:
+        DrawLabel("Collect the picture frame fragment in order\nto move to the next room", (Vector2){0, 0}, 40,
+                  RAYWHITE);
         break;
-      case Weaknesses:
-        break;
-      case CastTime:
-        break;
-      case Merging:
+      case FloorChange:
+        timePassed += GetFrameTime();
+        if (timePassed > 4) {
+          floor++;
+          stage = Floors;
+          fragment_location = gen_map(map);
+        }
+        DrawRectangle(0, 0, 720, 720, (Color){20, 10, 10, 255 - 255 * 0.25 * (timePassed - 2) * (timePassed - 2)});
+        DrawLabel("Completed Floor", (Vector2){260, 360}, 40,
+                  (Color){255, 255, 255, 255 - 255 * 0.25 * (timePassed - 2) * (timePassed - 2)});
         break;
       case Floors:
+        char str[20] = "Floor ";
+        snprintf(&str[6], 14, "%d", floor + 1);
+        DrawLabel(str, (Vector2){0, 0}, 40, RAYWHITE);
         break;
       case Victory:
+        DrawLabel("You Won", (Vector2){300, 300}, 40, RAYWHITE);
+        DrawLabel("You Broke The Hex", (Vector2){220, 360}, 40, RAYWHITE);
+        DrawLabel("You Got Your Cats Back", (Vector2){180, 420}, 40, RAYWHITE);
         break;
       case Loss:
         break;
     }
 
-    if (stage > Attacks) {
+    if (stage >= Attacks) {
       DrawPoly((Vector2){610, 640}, 6, 40, 0, BLUE);
       DrawPoly((Vector2){680, 680}, 6, 40, 0, RED);
-      DrawPoly((Vector2){680, 600}, 6, 40, 0, BROWN);
 
       Vector2 mousePoint = GetMousePosition();
 
       if (CheckCollisionPointCircle(mousePoint, (Vector2){610, 640}, 40) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         TraceLog(LOG_WARNING, "BLUE_PRESSED");
-      } else if (CheckCollisionPointCircle(mousePoint, (Vector2){680, 680}, 40) &&
-                 IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        TraceLog(LOG_WARNING, "BE_PRESSED");
-      } else if (CheckCollisionPointCircle(mousePoint, (Vector2){680, 600}, 40) &&
-                 IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        TraceLog(LOG_WARNING, "ESSED");
+        if (stage == Attacks)
+          stage++;
+      }
+      if (CheckCollisionPointCircle(mousePoint, (Vector2){680, 680}, 40) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        TraceLog(LOG_TRACE, "Dashed");
+        player.speedMultiplier = 10;
       }
     }
 
     EndDrawing();
   }
 
-  gen_map(map);
-  gen_map(map);
-
-  UnloadFont(iosevka);
+  UnloadFont(iosevka40);
+  UnloadFont(iosevka20);
   CloseWindow();
 
   return 0;
 }
 
-void draw_start_animation(float *timePassedptr, GameStage *stage) {
-  float timePassed = *timePassedptr;
+bool draw_start_animation(float timePassed) {
   if (timePassed > 10.0) {
-    (*stage)++;
-    timePassed = 0.0;
+    return true;
   } else if (timePassed > 6.0) {
     DrawCircleV((Vector2){360, 360}, 40, (Color){0, 0, 0, 255});
 
@@ -218,4 +271,5 @@ void draw_start_animation(float *timePassedptr, GameStage *stage) {
     DrawCircleV((Vector2){320, 390}, 20, (Color){0, 0, 0, 255});
     DrawCircleV((Vector2){400, 310}, 20, (Color){0, 0, 0, 255});
   }
+  return false;
 }
